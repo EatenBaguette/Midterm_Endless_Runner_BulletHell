6eecdae4ebc1f55f599910de44d9a44af67faf7b
EatenBaguette <esb1212@icloud.com>
2024-10-17 21:50:58 -0400

    fixed score system

--- Bullet Hell.nosync/Assets/Scripts/ScoreBehavior.cs
+++ Bullet Hell.nosync/Assets/Scripts/ScoreBehavior.cs
@@ -1,31 +1,29 @@
+using System;
 using UnityEngine;
 using TMPro;
 public class ScoreBehavior : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _scoreUI;
 
-    [SerializeField] private float _scoreMultiplier = 1f;
+    [SerializeField] private float _scoreMultiplier;
+    
+    private float _oldRawScore;
+    private float _newRawScore;
+    private float _multipliedScore;
 
-    private int _currentScore = 0;
-    private int _lastScore = 0;
-    private int _additionalScore;
-    private int _score = 0;
+    private int _score;
+    
     public float Score
     {
         get => _score;
 
         set
         {
-            _lastScore = _currentScore;
-            _currentScore = Mathf.FloorToInt(value * _scoreMultiplier);
+            _oldRawScore = _newRawScore;
+            _multipliedScore += ((value - _oldRawScore)) * _scoreMultiplier;
+            _score = Mathf.FloorToInt(_multipliedScore);
+            _newRawScore = value;
 
-            if (_lastScore > _currentScore)
-            {
-                _additionalScore = _lastScore - _currentScore;
-            }
-            
-            _score = _currentScore + _additionalScore;
-            
             _scoreUI.text = _score.ToString();
         }
     }
